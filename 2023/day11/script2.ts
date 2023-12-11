3824dd74e1d8d1729201bfddd19f8079da12287c
Han003 <jagolegion@gmail.com>
2023-12-11 16:13:15 +0100

    initial

--- 2023/day11/script2.ts
+++ 2023/day11/script2.ts
@@ -1,25 +1,20 @@
 (function() {
     const lines = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8').split(/\r?\n/).filter((l: any) => l?.length) as string[];
     const start = performance.now();
-    console.log(`time`, performance.now() - start);
 
     const universeWidth = lines[0].length;
-    console.log(`universeWidth`, universeWidth);
     const universeHeight = lines.length;
-    console.log(`universeHeight`, universeHeight);
-    const expansionSize = 1;
+    const expansionSize: number = 1e6;
 
     function findPairs<T>(array: T[]) {
         return ([] as T[][]).concat(...array.map(
             (v, i) => array.slice(i + 1).map((w) => [ v, w ])),
         );
-
     }
 
     function findDistance(g1: Galaxy, g2: Galaxy) {
         const yDiff = Math.abs((g1.y) - (g2.y));
         const xDiff = Math.abs((g1.x) - (g2.x));
-
         return xDiff + yDiff;
     }
 
@@ -76,22 +71,14 @@
     });
 
     Object.values(galaxies).forEach((galaxy) => {
-        if (expansionSize === 1) {
-            galaxy.x = galaxy.x + galaxy.xExpansions;
-            galaxy.y = galaxy.y + galaxy.yExpansions;
-        } else {
-            galaxy.x = (galaxy.x + (galaxy.xExpansions * expansionSize)) - galaxy.xExpansions;
-            galaxy.y = (galaxy.y + (galaxy.yExpansions * expansionSize)) - galaxy.yExpansions;
-        }
+        galaxy.x = (galaxy.x + (galaxy.xExpansions * expansionSize)) - (expansionSize === 1 ? 0 : galaxy.xExpansions);
+        galaxy.y = (galaxy.y + (galaxy.yExpansions * expansionSize)) - (expansionSize === 1 ? 0 : galaxy.yExpansions);
     });
-
-    console.log(`expansions`, expansions);
-    console.log(`galaxies`, galaxies);
-    console.log(`number of galaxies`, Object.keys(galaxies).length);
 
     const sum = findPairs(Object.keys(galaxies).map((galaxyId) => galaxyId)).reduce((acc, [ g1, g2 ]) => {
         return acc + findDistance(galaxies[g1], galaxies[g2]);
     }, 0);
 
     console.log(`sum`, sum);
+    console.log(`time`, performance.now() - start);
 })();
