834c2b2b4f4a0431bd9157d0cb5c19a35cf37d92
Han003 <jagolegion@gmail.com>
2023-12-12 14:36:29 +0100

    initial

--- 2023/day12/script.ts
+++ 2023/day12/script.ts
@@ -5,26 +5,36 @@
     const OPERATIONAL = '.';
     const DAMAGED = '#';
 
-    function getTargetConfig(line: string) {
-        const [ _, configData ] = line.split(' ');
-        return configData.split(',').map((x) => parseInt(x));
+    interface SpringConfig {
+        springData: string;
+        config: number[];
     }
 
-    function getAllPossibleLines(line: string, config: number[]) {
+    function getConfig(line: string, unfold: boolean): SpringConfig {
+        const [ springData, configData ] = line.split(' ');
+
+        return {
+            springData: Array.from({ length: unfold ? 5 : 1 }, () => 0).reduce((acc, _, i) => acc.concat(springData), [] as string[]).join(unfold ? '?' : ''),
+            config: Array.from({ length: unfold ? 5 : 1 }, () => configData.split(',').map((x) => parseInt(x))).flat(),
+        };
+    }
+
+    function getAllPossibleLines(config: SpringConfig) {
         let possibilities = 0;
-        const unknowns = line.replaceAll(/[^\?]/g, '').length;
+        const unknowns = config.springData.split('').filter((char) => char === '?').length;
         const number = parseInt('1'.repeat(unknowns), 2) + 1;
+        console.log(`number`, number);
 
         for (let i = 0; i < number; i++) {
-            let newLine = line;
+            let newLine = config.springData;
             const binaryNumber = i.toString(2);
-            const replacementArray = (''.padStart(unknowns - binaryNumber.length, '0') + binaryNumber).replaceAll('0', OPERATIONAL).replaceAll('1', DAMAGED).split('');
+            const replacements = (''.padStart(unknowns - binaryNumber.length, '0') + binaryNumber).replaceAll('0', OPERATIONAL).replaceAll('1', DAMAGED);
 
-            replacementArray.forEach((char) => {
-                newLine = newLine.replace('?', char);
-            });
+            for (let i = 0; i < replacements.length; i++) {
+                newLine = newLine.replace('?', replacements[i]);
+            }
 
-            if (lineMatchesConfig(newLine, config)) {
+            if (lineMatchesConfig(newLine, config.config)) {
                 possibilities++;
             }
         }
@@ -49,8 +59,11 @@
 
     let arrangements = 0;
 
-    lines.forEach((line) => {
-        arrangements += getAllPossibleLines(line, getTargetConfig(line));
+    [ '???.### 1,1,3' ].forEach((line) => {
+        const config = getConfig(line, true);
+        console.log(`config`, config);
+
+        arrangements += getAllPossibleLines(config);
     });
 
     console.log(`arrangements`, arrangements);
