58669baf820bf1de0cf85a97c08987d77537c958
han003 <joar.jacobsen@live.no>
2023-12-12 19:47:33 +0100

    Initial

--- 2023/day12/script.ts
+++ 2023/day12/script.ts
@@ -1,68 +1,56 @@
-(function() {
+(function () {
     const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8') as string;
     const lines = input.split(/\r?\n/).filter((l) => l.length) as string[];
     const start = performance.now();
     const OPERATIONAL = '.';
+    const UNKNOWN = '?';
     const DAMAGED = '#';
 
     interface SpringConfig {
-        springData: string;
-        config: number[];
+        line: string;
+        blocks: { size: number, intervals: {from: number, to: number}[] }[];
     }
 
     function getConfig(line: string, unfold: boolean): SpringConfig {
-        const [ springData, configData ] = line.split(' ');
+        const [springData, configData] = line.split(' ');
 
         return {
-            springData: Array.from({ length: unfold ? 5 : 1 }, () => 0).reduce((acc, _, i) => acc.concat(springData), [] as string[]).join(unfold ? '?' : ''),
-            config: Array.from({ length: unfold ? 5 : 1 }, () => configData.split(',').map((x) => parseInt(x))).flat(),
+            line: Array.from({length: unfold ? 5 : 1}, () => 0).reduce((acc, _, i) => acc.concat(springData), [] as string[]).join(unfold ? '?' : ''),
+            blocks: Array.from({length: unfold ? 5 : 1}, () => configData.split(',').map((x) => parseInt(x))).flat().map((size) => {
+                return {size, intervals: []};
+            }),
         };
     }
 
-    function getAllPossibleLines(config: SpringConfig) {
-        let possibilities = 0;
-        const unknowns = config.springData.split('').reduce((acc, char, index) => {
-            if (char === '?') {
-                acc.push(index);
-            }
-
-            return acc;
-        }, [] as number[]);
-
-        console.log(`unknowns`, unknowns);
-        const number = parseInt('1'.repeat(unknowns.length), 2) + 1;
-        console.log(`number`, number);
-
-        for (let i = 0; i < number; i++) {
-            let newLine = config.springData;
-            const binaryNumber = i.toString(2);
-            const replacements = (''.padStart(unknowns.length - binaryNumber.length, '0') + binaryNumber).replaceAll('0', OPERATIONAL).replaceAll('1', DAMAGED);
-
-            for (let i = 0; i < replacements.length; i++) {
-                newLine = newLine.replace('?', replacements[i]);
-            }
-
-            if (lineMatchesConfig(newLine, config.config)) {
-                possibilities++;
-            }
-        }
-
-        return possibilities;
-    }
-
-    function lineMatchesConfig(line: string, config: number[]) {
-        const split = line.split('.').filter((x) => x.length);
-        return split.every((x, i) => x.length === config[i]);
-    }
-
     let arrangements = 0;
+    const string = '.??..??...?## 1,1,3';
+    const config = getConfig(string, false);
 
-    [ '???.### 1,1,3' ].forEach((line) => {
-        const config = getConfig(line, true);
-        console.log(`config`, config);
+    console.log(`config`, config);
 
-        arrangements += getAllPossibleLines(config);
+    config.blocks.forEach(block => {
+        const intervals: number[][] = [];
+
+        console.log(`block`, block);
+        // Find where block can exist on the
+        config.line.split('').forEach((_, i, arr) => {
+            const previous = (i > 0 ? [arr[i - 1]] : []).map((x) => x === UNKNOWN ? OPERATIONAL : x);
+            const current = arr.slice(i, i + block.size).map((x) => x === UNKNOWN ? DAMAGED : x);
+            const next = arr.slice(i + block.size, i + block.size + 1).map((x) => x === UNKNOWN ? OPERATIONAL : x);
+            console.log(previous, current, next);
+
+            const fits = current.length === block.size && current.every((x) => x === DAMAGED) && next[0] !== DAMAGED && previous[0] !== DAMAGED;
+            if (fits) {
+                console.log(`fits`, i, i + block.size);
+                block.intervals.push({from: i, to: i + block.size});
+            }
+        });
     });
+
+    console.log(`config`, config);
+
+
+
 
     console.log(`arrangements`, arrangements);
     console.log(`time`, performance.now() - start);
