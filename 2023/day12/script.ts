4771f3e7141cc71c64a0423b3610920d11111f93
Han003 <jagolegion@gmail.com>
2023-12-12 10:42:01 +0100

    initial

--- 2023/day12/script.ts
+++ 2023/day12/script.ts
@@ -2,13 +2,20 @@
     const input = require('fs').readFileSync(require('path').resolve(__dirname, 'example-input.txt'), 'utf-8') as string;
     const lines = input.split(/\r?\n/).filter((l) => l.length) as string[];
     const start = performance.now();
+    const OPERATIONAL = '.';
+    const DAMAGED = '#';
+    const UNKNOWN = '?';
 
     function getSpringsAndTargetConfig(line: string) {
-        const [ springs, config ] = line.split(' ');
+        const [ springs, configData ] = line.split(' ');
+        const config = configData.split(',').map((x) => parseInt(x));
+        const brokenSprings = springs.split(OPERATIONAL).filter((x) => x.length);
+        console.log(`brokenSprings`, brokenSprings);
 
         return {
             springs,
-            config: config.split(',').map((x) => parseInt(x)),
+            config,
+            matches: brokenSprings.every((brokenSpring, i) => brokenSpring.length === config[i]),
         };
     }
 
@@ -17,5 +24,4 @@
     });
 
     console.log(`time`, performance.now() - start);
-    console.log(`sum`, sum);
 })();
---
+++ 2023/day12/script2.ts
@@ -0,0 +1,86 @@
+(function() {
+    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8') as string;
+    const lines = input.split(/\r?\n/).filter((l) => l.length) as string[];
+    const start = performance.now();
+
+    const universeWidth = lines[0].length;
+    const universeHeight = lines.length;
+    const expansionSize: number = 1e6;
+    const galaxyMatches = input.replaceAll(/\r?\n/g, '').matchAll(/#/g);
+
+    type Galaxy = { id: string, x: number, y: number, xExpansions: number, yExpansions: number };
+    const galaxies = Array.from(galaxyMatches).reduce((acc, match, galaxyIndex) => {
+        const matchIndex = match.index || 0;
+        const id = String(galaxyIndex);
+
+        acc[id] = {
+            id,
+            x: matchIndex % universeWidth,
+            y: Math.floor(matchIndex / universeWidth),
+            xExpansions: 0,
+            yExpansions: 0,
+        };
+
+        return acc;
+    }, {} as Record<string, Galaxy>);
+
+    function findPairs<T>(array: T[]) {
+        return ([] as T[][]).concat(...array.map(
+            (v, i) => array.slice(i + 1).map((w) => [ v, w ])),
+        );
+    }
+
+    function findDistance(g1: Galaxy, g2: Galaxy) {
+        const yDiff = Math.abs((g1.y) - (g2.y));
+        const xDiff = Math.abs((g1.x) - (g2.x));
+        return xDiff + yDiff;
+    }
+
+    // Find expansions
+    type Expansion = { x?: number, y?: number };
+    const expansions: Expansion[] = [];
+    for (let i = 0; i < universeWidth; i++) {
+        const empty = !Object.values(galaxies).find((g) => g.x === i);
+        if (empty) {
+            expansions.push({ x: i });
+        }
+    }
+    for (let i = 0; i < universeHeight; i++) {
+        const empty = !Object.values(galaxies).find((g) => g.y === i);
+        if (empty) {
+            expansions.push({ y: i });
+        }
+    }
+
+    // Add expansions to galaxies
+    expansions.forEach((expansion) => {
+        if (expansion.x) {
+            Object.values(galaxies).forEach((galaxy) => {
+                if (galaxy.x > expansion.x!) {
+                    galaxy.xExpansions++;
+                }
+            });
+        }
+
+        if (expansion.y) {
+            Object.values(galaxies).forEach((galaxy) => {
+                if (galaxy.y > expansion.y!) {
+                    galaxy.yExpansions++;
+                }
+            });
+        }
+    });
+
+    // Update galaxy positions
+    Object.values(galaxies).forEach((galaxy) => {
+        galaxy.x = (galaxy.x + (galaxy.xExpansions * expansionSize)) - (expansionSize === 1 ? 0 : galaxy.xExpansions);
+        galaxy.y = (galaxy.y + (galaxy.yExpansions * expansionSize)) - (expansionSize === 1 ? 0 : galaxy.yExpansions);
+    });
+
+    const sum = findPairs(Object.keys(galaxies).map((galaxyId) => galaxyId)).reduce((acc, [ g1, g2 ]) => {
+        return acc + findDistance(galaxies[g1], galaxies[g2]);
+    }, 0);
+
+    console.log(`time`, performance.now() - start);
+    console.log(`sum`, sum);
+})();
