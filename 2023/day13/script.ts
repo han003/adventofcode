e45731bddb9a312111e0c565b194e10d49a78727
han003 <joar.jacobsen@live.no>
2023-12-13 17:30:02 +0100

    Initial

--- 2023/day13/script.ts
+++ 2023/day13/script.ts
@@ -13,94 +13,70 @@
         currentIndex = indexes.shift();
     }
 
-    function findOkVerticalIndexes(group: string[]) {
-        const leftSide = group.reduce((acc, line) => acc + line[0], '');
-        const rightSide = group.reduce((acc, line) => acc + line[line.length - 1], '');
+    function rotateN90(a: string[][]) {
+        var temp = new Array(a[0].length); // number of columns
+        var i = 0;
 
-        // Left to right
-        const possibleTopIndexes = findOkHorizontalIndexes(leftSide, '.');
-        const possibleBottomIndexes = findOkHorizontalIndexes(rightSide, '#');
-        let possibleHorizontalPaths = possibleTopIndexes.filter((topIndex) => possibleBottomIndexes.includes(topIndex));
+        for (i = 0; i < temp.length; i++) {
+            temp[i] = [];
+        }
 
-        // Right to left
-        const possibleTopIndexes2 = findOkHorizontalIndexes(leftSide, '#');
-        const possibleBottomIndexes2 = findOkHorizontalIndexes(rightSide, '.');
-        possibleHorizontalPaths = possibleTopIndexes2.filter((topIndex) => possibleBottomIndexes2.includes(topIndex)).concat(possibleHorizontalPaths);
+        for (i = 0; i < a.length; i++) {
 
-        return possibleHorizontalPaths;
+            for (let j = 0; j < a[0].length; j++) {
+
+                temp[j][i] = a[i][a[i].length - 1 - j];
+            }
+        }
+
+        return temp;
     }
 
-    function findOkHorizontalIndexes(line: string, target: '.' | '#') {
-        return line.split('').reduce((acc, char, index, arr) => {
-            if (index === arr.length - 1) return acc;
+    function checkMirrorable(groupArr: string[][], lineIndex: number, charIndex: number, increment: number) {
+        if (charIndex === 0) {
+            return checkMirrorable(groupArr, lineIndex, charIndex + 1, 0);
+        }
 
-            const nextChar = arr[index + 1];
-            if (char === target && nextChar === target) {
-                acc.push(index);
+        const line = groupArr[lineIndex];
+
+        console.log(`line`, line.join(''));
+        console.log(`line char`, lineIndex, charIndex);
+
+        const prev = line[charIndex - 1 - increment];
+        const curr = line[charIndex + increment];
+
+        console.log(`prev and curr`, prev, curr);
+
+        // Line finished successfully, check next line
+        if (prev === undefined || curr === undefined) {
+            if (lineIndex === groupArr.length - 1) {
+                return charIndex;
             }
 
-            return acc;
-        }, [] as number[]);
-    }
-
-    function getPossiblePaths(group: string[]) {
-        // Top to bottom
-        const possibleTopIndexes = findOkHorizontalIndexes(group[0], '.');
-        const possibleBottomIndexes = findOkHorizontalIndexes(group[group.length - 1], '#');
-        let possibleVerticalPaths = possibleTopIndexes.filter((topIndex) => possibleBottomIndexes.includes(topIndex));
-
-        // Bottom to top
-        const possibleTopIndexes2 = findOkHorizontalIndexes(group[0], '#');
-        const possibleBottomIndexes2 = findOkHorizontalIndexes(group[group.length - 1], '.');
-        possibleVerticalPaths = possibleTopIndexes2.filter((topIndex) => possibleBottomIndexes2.includes(topIndex)).concat(possibleVerticalPaths);
-
-        const res = {
-            // Check down
-            vertical: new Set(possibleVerticalPaths),
-            // Check right
-            horizontal: new Set(findOkVerticalIndexes(group))
+            return checkMirrorable(groupArr, lineIndex + 1, charIndex, 0);
         }
 
-        return res;
-    }
-
-    function getDistanceToMirrors(line: string, path: number) {
-        console.log(`-------------------------`,);
-
-        console.log(`line`, line);
-
-        if (line[path] === '#' && line[path + 1] === '#') {
-            return [0, 0];
+        // If chars are the same, check next char
+        if (prev === curr) {
+            return checkMirrorable(groupArr, lineIndex, charIndex, increment + 1);
+        } else { // Or update char index and check
+            return checkMirrorable(groupArr, lineIndex, charIndex + 1, 0);
         }
 
-        const left = line.slice(0, path).split('').reverse().join('');
-        const leftIndex = left.indexOf('#') + 1;
-
-
-        return [leftIndex, line.slice(path).indexOf('#') - 1];
-    }
-
-    function checkVerticalPath(group: string[], path: number) {
-        console.log(group);
-        console.log(`path`, path);
-
-        return group.every((line) => {
-            const distance = getDistanceToMirrors(line, path);
-            return distance[0] === distance[1];
-        });
+        return false;
     }
 
     groups.slice(0, 1).forEach((group) => {
-        console.log(`group`, group);
+        const groupArr = group.map((line) => line.split(''))
+        console.log(`groupArr`, groupArr);
 
-        const paths = getPossiblePaths(group);
-        console.log(`paths`, paths);
+        const mirrorable = checkMirrorable(groupArr, 0, 0, 0)
+        console.log(`mirrorable`, mirrorable);
 
-        paths.vertical.forEach((path) => {
-            const isValid = checkVerticalPath(group, path)
-            console.log(`VERTICAL isValid`, isValid, path);
-        });
+        // const rotated = rotateN90(groupArr);
+        // console.log(`rotated`, rotated);
     });
+
 
     console.log(`time`, performance.now() - start);
 })();
