c80001fcf16c66cb13c31de5fb4d2cda38921d8e
Han003 <jagolegion@gmail.com>
2023-12-14 10:31:12 +0100

    14-1 done

---
+++ 2023/day14/script.ts
@@ -0,0 +1,42 @@
+(function() {
+    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8') as string;
+    const lines = (input.split(/\r?\n/) as string[]).filter((l) => l.length).map((l) => l.split(''));
+    const start = performance.now();
+    const ROUNDROCK = 'O';
+    const CUBEROCK = '#';
+    const EMPTY = '.';
+
+    function moveRoundRock(row: number, col: number) {
+        if (row === 0) {
+            return;
+        }
+
+        if (lines[row - 1][col] === EMPTY) {
+            lines[row - 1][col] = ROUNDROCK;
+            lines[row][col] = EMPTY;
+            moveRoundRock(row - 1, col);
+        }
+    }
+
+    for (let row = 0; row < lines.length; row++) {
+        for (let col = 0; col < lines[row].length; col++) {
+            if (lines[row][col] !== ROUNDROCK) {
+                continue;
+            }
+
+            moveRoundRock(row, col);
+        }
+    }
+
+    // lines.forEach((line) => console.log(line.join('')));
+
+    const sum = lines.reduce((acc, line, index, arr) => {
+        const lineValue = arr.length - index;
+        const lineItems = line.filter((x) => x === ROUNDROCK).length;
+        return acc + lineValue * lineItems;
+    }, 0);
+
+    console.log(`sum`, sum);
+
+    console.log(`time`, performance.now() - start);
+})();
