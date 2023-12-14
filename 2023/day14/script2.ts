07b51d3d48ee6340b759b03d8aab7e53fa2baa16
Han003 <jagolegion@gmail.com>
2023-12-14 15:35:04 +0100

    test

--- 2023/day14/script2.ts
+++ 2023/day14/script2.ts
@@ -5,19 +5,30 @@
     const ROUNDROCK = 'O';
     const CUBEROCK = '#';
     const EMPTY = '.';
-    const CYCLES = 1;
+    const CYCLES = 1000; // 1_000_000_000;
     const NORTH = 1;
     const SOUTH = -1;
     const EAST = -1;
     const WEST = 1;
 
+    function getPattern() {
+        return lines.reduce((acc, line) => {
+            return acc + line.join('');
+        }, '');
+    }
+
+    function getSum() {
+        return lines.reduce((acc, line, index, arr) => {
+            const lineValue = arr.length - index;
+            const lineItems = line.filter((x) => x === ROUNDROCK).length;
+            return acc + lineValue * lineItems;
+        }, 0);
+    }
+
     function moveRoundRock(rowDirection: typeof NORTH | typeof SOUTH | 0, columnDirection: typeof WEST | typeof EAST | 0, row: number, col: number) {
         if (!lines[row - rowDirection]) {
             return;
         }
-
-        console.log(`move rock at`, row, col);
-        console.log(`lines[row - rowDirection][col - columnDirection]`, lines[row - rowDirection][col - columnDirection]);
 
         if (lines[row - rowDirection][col - columnDirection] === EMPTY) {
             lines[row - rowDirection][col - columnDirection] = ROUNDROCK;
@@ -29,59 +40,56 @@
     const rows = lines.length;
     const columns = lines[0].length;
 
-    // North
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < columns; col++) {
-            if (lines[row][col] !== ROUNDROCK) {
-                continue;
-            }
-
-            // moveRoundRock(1, 0, row, col);
-        }
-    }
-
-    // South
-    for (let row = rows - 1; row >= 0; row--) {
-        for (let col = 0; col < columns; col++) {
-            if (lines[row][col] !== ROUNDROCK) {
-                continue;
-            }
-
-            // moveRoundRock(-1, 0, row, col);
-        }
-    }
-
-    // East
-    for (let col = columns - 1; col >= 0; col--) {
+    for (let i = 0; i < CYCLES; i++) {
+        // North
         for (let row = 0; row < rows; row++) {
-            if (lines[row][col] !== ROUNDROCK) {
-                continue;
-            }
+            for (let col = 0; col < columns; col++) {
+                if (lines[row][col] !== ROUNDROCK) {
+                    continue;
+                }
 
-            // moveRoundRock(0, -1, row, col);
+                moveRoundRock(1, 0, row, col);
+            }
+        }
+
+        // West
+        for (let col = 0; col < columns; col++) {
+            for (let row = 0; row < rows; row++) {
+                if (lines[row][col] !== ROUNDROCK) {
+                    continue;
+                }
+
+                moveRoundRock(0, 1, row, col);
+            }
+        }
+
+        // South
+        for (let row = rows - 1; row >= 0; row--) {
+            for (let col = 0; col < columns; col++) {
+                if (lines[row][col] !== ROUNDROCK) {
+                    continue;
+                }
+
+                moveRoundRock(-1, 0, row, col);
+            }
+        }
+
+        // East
+        for (let col = columns - 1; col >= 0; col--) {
+            for (let row = 0; row < rows; row++) {
+                if (lines[row][col] !== ROUNDROCK) {
+                    continue;
+                }
+
+                moveRoundRock(0, -1, row, col);
+            }
+        }
+
+        if (getSum() === 64) {
+            lines.forEach((line) => console.log(line.join('')));
+            break;
         }
     }
-
-    // West
-    for (let col = 0; col < columns; col++) {
-        for (let row = 0; row < rows; row++) {
-            if (lines[row][col] !== ROUNDROCK) {
-                continue;
-            }
-
-            moveRoundRock(0, 1, row, col);
-        }
-    }
-
-    lines.forEach((line, index) => console.log(String(index + 1), line.join('')));
-
-    const sum = lines.reduce((acc, line, index, arr) => {
-        const lineValue = arr.length - index;
-        const lineItems = line.filter((x) => x === ROUNDROCK).length;
-        return acc + lineValue * lineItems;
-    }, 0);
-
-    console.log(`sum`, sum);
 
     console.log(`time`, performance.now() - start);
 })();
