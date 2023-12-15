f55692c197b99c0f85394da047bf9cd797741168
Han003 <jagolegion@gmail.com>
2023-12-15 11:48:12 +0100

    temp

--- 2023/day15/script2.ts
+++ 2023/day15/script2.ts
@@ -1,5 +1,5 @@
 (function() {
-    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'example-input.txt'), 'utf-8') as string;
+    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8') as string;
     const start = performance.now();
 
     function hashAlogorithm(string: string) {
@@ -19,60 +19,87 @@
     const lensLabelIndexesMap = new Map<string, number>();
     const lensLabelToBoxMap = new Map<string, number>();
     const boxes: string[][] = Array.from({ length: 256 }, () => []);
-    console.log(`boxes`, boxes);
     const hashList = input.replaceAll('\r\n', '').split(',');
-    console.log(`hashList`, hashList);
 
-    hashList.forEach((hash) => {
-        console.log(`${hash} --------------------------------`);
+    hashList.forEach((hash, i, arr) => {
+        console.log(i+1, '/', arr.length);
+        // console.log(`-------------------------------------------------`);
+        // console.log(`${hash} --------------------------------`);
+        // console.log(`-------------------------------------------------`);
+
+        // boxes.forEach((box, index) => {
+        //     if (box.length >= 1) {
+        //         console.log(`box`, index, box);
+        //     }
+        // });
 
         if (hash.includes('-')) {
             const [ lensLabel, focalStrength ] = hash.split('-');
-            const box = hashAlogorithm(lensLabel);
-            console.log(`box`, box);
-            console.log(`lensLabel`, lensLabel);
-            console.log(`focalStrength`, focalStrength);
+            const currentBox = hashAlogorithm(lensLabel);
+            // console.log(`box`, currentBox);
+            // console.log(`lensLabel`, lensLabel);
+            // console.log(`focalStrength`, focalStrength);
 
-            const sameBox = lensLabelToBoxMap.get(lensLabel) === box;
-            console.log(`sameBox`, sameBox);
+            const sameBox = lensLabelToBoxMap.get(lensLabel) === currentBox;
+            // console.log(`sameBox`, sameBox);
             if (sameBox) {
-                console.log(`remove lens from`, box);
+                // console.log(`remove lens from`, currentBox);
                 const lensIndex = lensLabelIndexesMap.get(lensLabel)!;
-                console.log(`lensIndex`, lensIndex);
-                boxes[box].splice(lensIndex, 1);
+                // console.log(`lensIndex`, lensIndex);
+                boxes[currentBox].splice(lensIndex, 1);
 
                 lensLabelToBoxMap.delete(lensLabel);
                 lensLabelIndexesMap.delete(lensLabel);
-                console.log(`lensLabelIndexesMap`, lensLabelIndexesMap);
+                // console.log(`lensLabelIndexesMap`, lensLabelIndexesMap);
+
+                Array.from(lensLabelToBoxMap.entries()).forEach(([ lensLabel, box ]) => {
+                    if (box === currentBox) {
+                        // console.log(`UPDATE INDEX OF`, lensLabel);
+                        const oldIndex = lensLabelIndexesMap.get(lensLabel)!;
+                        lensLabelIndexesMap.set(lensLabel, oldIndex - 1);
+                    }
+                });
             }
         } else {
             const [ lensLabel, focalStrength ] = hash.split('=');
-            const box = hashAlogorithm(lensLabel);
+            const currentBox = hashAlogorithm(lensLabel);
             const lensLabelWithFocal = `${lensLabel} ${focalStrength}`;
 
             // If box has label already
             if (lensLabelToBoxMap.get(lensLabel)) {
-                console.log(`box`, box, 'already has', lensLabel);
+                // console.log(`box`, currentBox, 'already has', lensLabel);
                 const lensIndex = lensLabelIndexesMap.get(lensLabel)!;
-                boxes[box].splice(lensIndex, 1, lensLabelWithFocal);
+                // console.log(`replace`, 'at', lensIndex);
+                // console.log(`lensLabelIndexesMap`, lensLabelIndexesMap);
+                boxes[currentBox].splice(lensIndex, 1, lensLabelWithFocal);
             } else {
-                lensLabelToBoxMap.set(lensLabel, box);
-                lensLabelIndexesMap.set(lensLabel, boxes[box].push(lensLabelWithFocal) - 1);
+                lensLabelToBoxMap.set(lensLabel, currentBox);
+                lensLabelIndexesMap.set(lensLabel, boxes[currentBox].push(lensLabelWithFocal) - 1);
+                // console.log(`lensLabelIndexesMap`, lensLabelIndexesMap);
             }
         }
 
-        boxes.forEach((box, index) => {
-            if (box.length >= 1) {
-                console.log(`box`, index, box);
-            }
+        // boxes.forEach((box, index) => {
+        //     if (box.length >= 1) {
+        //         console.log(`box`, index, box);
+        //     }
+        // });
+    });
+
+    let sum = 0;
+    boxes.forEach((box, boxIndex) => {
+        if (box.length === 0) {
+            return;
+        }
+
+        console.log(`box`, boxIndex, box);
+        box.forEach((lens, lensIndex) => {
+            const [ lensLabel, focalStrength ] = lens.split(' ');
+            sum += (1 + boxIndex) * (lensIndex + 1) * parseInt(focalStrength);
         });
     });
 
-    boxes.forEach((box, index) => {
-        if (box.length > 1) {
-            console.log(`box`, index, box);
-        }
-    });
+    console.log(`sum`, sum);
 
     console.log(`time`, performance.now() - start);
 })();
