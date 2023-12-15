65b540309299a4d164d27ae192701ebb9f278f9a
Han003 <jagolegion@gmail.com>
2023-12-15 10:09:12 +0100

    15-1 done

---
+++ 2023/day15/script2.ts
@@ -0,0 +1,32 @@
+(function() {
+    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8') as string;
+    const start = performance.now();
+
+    function hashAlogorithm(string: string) {
+        let currentValue = 0;
+
+        for (let i = 0; i < string.length; i++) {
+            const charCode = string.charCodeAt(i);
+
+            currentValue += charCode;
+            currentValue *= 17;
+            currentValue %= 256;
+        }
+
+        console.log(string, currentValue);
+
+        return currentValue;
+    }
+
+    const hashList = input.replaceAll('\r\n', '').split(',');
+    console.log(`hashList`, hashList);
+
+    let sum = 0;
+    hashList.forEach((hash) => {
+        sum += hashAlogorithm(hash);
+    });
+
+    console.log(`sum`, sum);
+
+    console.log(`time`, performance.now() - start);
+})();
