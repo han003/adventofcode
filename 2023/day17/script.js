735b1011ae6d9ab7880a20496155c11bbd252c94
han003 <joar.jacobsen@live.no>
2023-12-17 16:50:45 +0100

    17-1 test

--- 2023/day17/script.js
+++ 2023/day17/script.js
@@ -6,70 +6,113 @@
     const board = lines.map(l => l.split('').map(s => parseInt(s)));
     let activeEmitters = 0;
     let currentLowest = Infinity;
+    let iterations = 0;
     class Emitter {
-        constructor(board, row, column, visited, values, path, largestRow, largestColumn) {
+        constructor(board, row, column, visited, sum, path, largestRow, largestColumn) {
+            this.board = board;
             this.row = row;
             this.column = column;
             this.visited = visited;
-            this.values = values;
+            this.sum = sum;
+            this.path = path;
             this.largestRow = largestRow;
             this.largestColumn = largestColumn;
             activeEmitters++;
-            console.log(`--------------------------------------`);
-            if (row === board.length - 1 && column === board[0].length - 1) {
-                console.log(`DONE THIS`, this.getSum());
-                if (this.getSum() < currentLowest) {
-                    currentLowest = this.getSum();
+            iterations++;
+            if (row === board.length - 1 && column === board[0].length) {
+                // console.log(`DONE THIS`, this.sum);
+                // console.log(`row`, row);
+                // console.log(`column`, column);
+                if (this.sum < currentLowest) {
+                    currentLowest = this.sum;
+                    this.drawPath();
                 }
                 activeEmitters--;
                 return;
             }
             const tileValue = board[row]?.[column];
             if (tileValue == null) {
-                console.log(`No tile`);
+                // console.log(`No tile`,);
                 activeEmitters--;
                 return;
             }
             if (visited.has(this.getTileKey())) {
-                console.log(`already visited`, this.getTileKey());
+                // console.log(`already visited`, this.getTileKey());
                 activeEmitters--;
                 return;
             }
             if (row < largestRow && (largestRow - row) > 1) {
-                console.log(`too far up`);
+                // console.log(`too far up`);
                 activeEmitters--;
                 return;
             }
             if (column < largestColumn && (largestColumn - column) > 1) {
-                console.log(`too far left`);
+                // console.log(`too far left`);
                 activeEmitters--;
                 return;
             }
-            if (this.getSum() >= currentLowest) {
-                console.log(`sum will exceed`);
+            if (this.sum >= currentLowest) {
+                // console.log(`sum will exceed`);
+                activeEmitters--;
                 return;
             }
+            const newSum = sum + tileValue;
             visited.set(this.getTileKey(), tileValue);
             // console.log(`this`, this);
-            console.log(`activeEmitters`, activeEmitters);
-            if (!this.hasThreeLeftPrevious) {
-                new Emitter(board, row, column - 1, new Map(visited), values.concat(tileValue), path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
+            // console.log(`activeEmitters`, activeEmitters);
+            if (iterations > 100000000) {
+                // this.drawPath();
+                return;
             }
-            if (!this.hasThreeRightPrevious) {
-                new Emitter(board, row, column + 1, new Map(visited), values.concat(tileValue), path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
+            if (this.canGoLeft) {
+                new Emitter(board, row, column - 1, new Map(visited), newSum, path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
             }
-            if (!this.hasThreeDownPrevious) {
-                new Emitter(board, row + 1, column, new Map(visited), values.concat(tileValue), path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
+            if (this.canGoRight) {
+                new Emitter(board, row, column + 1, new Map(visited), newSum, path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
             }
-            if (!this.hasThreeUpPrevious) {
-                new Emitter(board, row - 1, column, new Map(visited), values.concat(tileValue), path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
+            if (this.canGoDown) {
+                new Emitter(board, row + 1, column, new Map(visited), newSum, path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
             }
+            if (this.canGoUp) {
+                new Emitter(board, row - 1, column, new Map(visited), newSum, path.concat(this.getTileKey()), Math.max(this.largestRow, row), Math.max(this.largestColumn, column));
+            }
+            activeEmitters--;
         }
-        getSum() {
-            return this.values.reduce((acc, v) => acc + v, 0);
+        drawPath() {
+            const path = this.path;
+            const board = structuredClone(this.board);
+            path.forEach((tileKey) => {
+                const [row, column] = tileKey.split(',').map(s => parseInt(s));
+                board[row][column] = '.';
+            });
+            console.group('Path');
+            board.forEach((row) => {
+                console.log(row.join(''));
+            });
+            console.groupEnd();
         }
         getTileKey(row, column) {
             return `${row ?? this.row},${column ?? this.column}`;
+        }
+        get canGoLeft() {
+            const hasAbove = this.visited.has(this.getTileKey(this.row - 1, this.column));
+            const hasAboveLeft = this.visited.has(this.getTileKey(this.row - 1, this.column - 1));
+            return !this.hasThreeLeftPrevious && !(hasAbove && hasAboveLeft);
+        }
+        get canGoRight() {
+            const hasAbove = this.visited.has(this.getTileKey(this.row - 1, this.column));
+            const hasAboveRight = this.visited.has(this.getTileKey(this.row - 1, this.column + 1));
+            return !this.hasThreeRightPrevious && !(hasAbove && hasAboveRight);
+        }
+        get canGoUp() {
+            const hasLeft = this.visited.has(this.getTileKey(this.row, this.column - 1));
+            const hasAboveLeft = this.visited.has(this.getTileKey(this.row - 1, this.column - 1));
+            return !this.hasThreeUpPrevious && !(hasLeft && hasAboveLeft);
+        }
+        get canGoDown() {
+            const hasLeft = this.visited.has(this.getTileKey(this.row, this.column - 1));
+            const hasBelowLeft = this.visited.has(this.getTileKey(this.row + 1, this.column - 1));
+            return !this.hasThreeDownPrevious && !(hasLeft && hasBelowLeft);
         }
         get hasThreeRightPrevious() {
             return this.visited.has(this.getTileKey(this.row, this.column - 1)) && this.visited.has(this.getTileKey(this.row, this.column - 2)) && this.visited.has(this.getTileKey(this.row, this.column - 3));
@@ -84,6 +127,10 @@
             return this.visited.has(this.getTileKey(this.row, this.column + 1)) && this.visited.has(this.getTileKey(this.row, this.column + 2)) && this.visited.has(this.getTileKey(this.row, this.column + 3));
         }
     }
-    new Emitter(board, 0, 0, new Map(), [], [], 0, 0);
+    new Emitter(board, 0, 0, new Map(), 0, [], 0, 0);
+    console.log(`Lowest:`, currentLowest);
+    console.log(`Iterations:`, iterations);
+    console.log(`Active emitters:`, activeEmitters);
+    console.log(`Time:`, performance.now() - start);
 })();
 //# sourceMappingURL=script.js.map
--- 2023/day17/script.ts
+++ 2023/day17/script.ts
@@ -5,26 +5,30 @@
     const board = lines.map(l => l.split('').map(s => parseInt(s)));
     let activeEmitters = 0;
     let currentLowest = Infinity;
+    let iterations = 0;
 
     class Emitter {
         constructor(
-            board: number[][],
+            private board: number[][],
             private row: number,
             private column: number,
             private visited: Map<string, number>,
-            private values: number[],
-            path: string[],
+            private sum: number,
+            private path: string[],
             private largestRow: number,
             private largestColumn: number,
         ) {
             activeEmitters++;
+            iterations++;
 
-            console.log(`--------------------------------------`, );
+            if (row === board.length - 1 && column === board[0].length) {
+                // console.log(`DONE THIS`, this.sum);
+                // console.log(`row`, row);
+                // console.log(`column`, column);
+                if (this.sum < currentLowest) {
+                    currentLowest = this.sum;
 
-            if (row === board.length - 1 && column === board[0].length - 1) {
-                console.log(`DONE THIS`, this.getSum());
-                if (this.getSum() < currentLowest) {
-                    currentLowest = this.getSum();
+                    this.drawPath();
                 }
 
                 activeEmitters--;
@@ -33,98 +37,144 @@
 
             const tileValue = board[row]?.[column];
             if (tileValue == null) {
-                console.log(`No tile`,);
+                // console.log(`No tile`,);
                 activeEmitters--;
                 return;
             }
 
             if (visited.has(this.getTileKey())) {
-                console.log(`already visited`, this.getTileKey());
+                // console.log(`already visited`, this.getTileKey());
                 activeEmitters--;
                 return
             }
 
             if (row < largestRow && (largestRow - row) > 1) {
-                console.log(`too far up`);
+                // console.log(`too far up`);
                 activeEmitters--;
                 return;
             }
 
             if (column < largestColumn && (largestColumn - column) > 1) {
-                console.log(`too far left`);
+                // console.log(`too far left`);
                 activeEmitters--;
                 return;
             }
 
-            if (this.getSum() >= currentLowest) {
-                console.log(`sum will exceed`);
+            if (this.sum >= currentLowest) {
+                // console.log(`sum will exceed`);
+                activeEmitters--;
                 return;
             }
 
+            const newSum = sum + tileValue;
             visited.set(this.getTileKey(), tileValue);
 
             // console.log(`this`, this);
-            console.log(`activeEmitters`, activeEmitters);
+            // console.log(`activeEmitters`, activeEmitters);
 
-            if (!this.hasThreeLeftPrevious) {
+
+            if (iterations > 100_000_000) {
+                // this.drawPath();
+                return;
+            }
+
+            if (this.canGoLeft) {
                 new Emitter(
                     board,
                     row,
                     column - 1,
                     new Map(visited),
-                    values.concat(tileValue),
+                    newSum,
                     path.concat(this.getTileKey()),
                     Math.max(this.largestRow, row),
                     Math.max(this.largestColumn, column),
                 );
             }
 
-            if (!this.hasThreeRightPrevious) {
+            if (this.canGoRight) {
                 new Emitter(
                     board,
                     row,
                     column + 1,
                     new Map(visited),
-                    values.concat(tileValue),
+                    newSum,
                     path.concat(this.getTileKey()),
                     Math.max(this.largestRow, row),
                     Math.max(this.largestColumn, column),
                 );
             }
 
-            if (!this.hasThreeDownPrevious) {
+            if (this.canGoDown) {
                 new Emitter(
                     board,
                     row + 1,
                     column,
                     new Map(visited),
-                    values.concat(tileValue),
+                    newSum,
                     path.concat(this.getTileKey()),
                     Math.max(this.largestRow, row),
                     Math.max(this.largestColumn, column),
                 );
             }
 
-            if (!this.hasThreeUpPrevious) {
+            if (this.canGoUp) {
                 new Emitter(
                     board,
                     row - 1,
                     column,
                     new Map(visited),
-                    values.concat(tileValue),
+                    newSum,
                     path.concat(this.getTileKey()),
                     Math.max(this.largestRow, row),
                     Math.max(this.largestColumn, column),
                 );
             }
+
+            activeEmitters--;
         }
 
-        getSum() {
-            return this.values.reduce((acc, v) => acc + v, 0);
+        drawPath() {
+            const path = this.path;
+            const board: (number | string)[][] = structuredClone(this.board);
+
+            path.forEach((tileKey) => {
+                const [row, column] = tileKey.split(',').map(s => parseInt(s));
+                board[row][column] = '.';
+            });
+
+            console.group('Path');
+            board.forEach((row) => {
+                console.log(row.join(''));
+            });
+            console.groupEnd();
         }
 
         getTileKey(row?: number, column?: number) {
             return `${row ?? this.row},${column ?? this.column}`;
+        }
+
+        get canGoLeft() {
+            const hasAbove = this.visited.has(this.getTileKey(this.row - 1, this.column));
+            const hasAboveLeft = this.visited.has(this.getTileKey(this.row - 1, this.column - 1));
+            return !this.hasThreeLeftPrevious && !(hasAbove && hasAboveLeft);
+        }
+
+        get canGoRight() {
+            const hasAbove = this.visited.has(this.getTileKey(this.row - 1, this.column));
+            const hasAboveRight = this.visited.has(this.getTileKey(this.row - 1, this.column + 1));
+            return !this.hasThreeRightPrevious && !(hasAbove && hasAboveRight);
+        }
+
+        get canGoUp() {
+            const hasLeft = this.visited.has(this.getTileKey(this.row, this.column - 1));
+            const hasAboveLeft = this.visited.has(this.getTileKey(this.row - 1, this.column - 1));
+            return !this.hasThreeUpPrevious && !(hasLeft && hasAboveLeft);
+        }
+
+        get canGoDown() {
+            const hasLeft = this.visited.has(this.getTileKey(this.row, this.column - 1));
+            const hasBelowLeft = this.visited.has(this.getTileKey(this.row + 1, this.column - 1));
+            return !this.hasThreeDownPrevious && !(hasLeft && hasBelowLeft);
         }
 
         get hasThreeRightPrevious() {
@@ -144,5 +194,10 @@
         }
     }
 
-    new Emitter(board, 0, 0, new Map(), [], [], 0, 0);
+    new Emitter(board, 0, 0, new Map(), 0, [], 0, 0);
+
+    console.log(`Lowest:`, currentLowest);
+    console.log(`Iterations:`, iterations);
+    console.log(`Active emitters:`, activeEmitters);
+    console.log(`Time:`, performance.now() - start);
 })();
