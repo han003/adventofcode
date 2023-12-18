2a4117cb722d1d42d172ab76d1129fdc3dc2d94b
Han003 <jagolegion@gmail.com>
2023-12-18 12:14:38 +0100

    dijkstra

--- 2023/day17/script.ts
+++ 2023/day17/script.ts
@@ -4,37 +4,33 @@
     const lines = (input.split(/\r?\n/) as string[]).filter((l) => l.length);
     const board = lines.map((l) => l.split('').map((s) => parseInt(s)));
 
-    function getNeighbors([ row, col ]: number[]) {
-        const neighbors: number[][] = [];
+    function getNeighbors(coords: Coords) {
+        const neighbors: Coords[] = [];
 
         // Up
-        if (board[row + 1]?.[col] != null) {
-            neighbors.push([ row + 1, col ]);
+        if (board[coords.row + 1]?.[coords.col] != null) {
+            neighbors.push(new Coords(coords.row + 1, coords.col));
         }
 
         // Down
-        if (board[row - 1]?.[col] != null) {
-            neighbors.push([ row - 1, col ]);
+        if (board[coords.row - 1]?.[coords.col] != null) {
+            neighbors.push(new Coords(coords.row - 1, coords.col));
         }
 
         // Left
-        if (board[row]?.[col - 1] != null) {
-            neighbors.push([ row, col - 1 ]);
+        if (board[coords.row]?.[coords.col - 1] != null) {
+            neighbors.push(new Coords(coords.row, coords.col - 1));
         }
 
         // Right
-        if (board[row]?.[col + 1] != null) {
-            neighbors.push([ row, col + 1 ]);
+        if (board[coords.row]?.[coords.col + 1] != null) {
+            neighbors.push(new Coords(coords.row, coords.col + 1));
         }
 
         return neighbors;
     }
 
     function reconstructPath(goal: string, start: string, path: Map<string, string>, board: (number | string)[][]) {
-        console.log(`goal`, goal);
-        console.log(`start`, start);
-        console.log(`path`, path);
-
         const boardCopy = structuredClone(board);
         let current = goal;
 
@@ -47,66 +43,47 @@
         boardCopy.forEach((row) => console.log(row.join(' ')));
     }
 
+    class Coords {
+        constructor(public row: number, public col: number) {
+        }
+
+        get key() {
+            return `${this.row},${this.col}`;
+        }
+    }
+
     function dijkstra() {
-        const goal = [ board.length - 1, board[0].length - 1 ];
+        const goal = new Coords(board.length - 1, board[0].length - 1);
         // const goal = [ 5, 11 ];
 
-        const priorityQueue: { row: number, col: number, priority: number, coords: string }[] = [];
-        priorityQueue.push({ row: 0, col: 0, priority: 0, coords: [ 0, 0 ].toString() });
+        const priorityQueue: { row: number, col: number, priority: number, coords: Coords }[] = [];
+        priorityQueue.push({ row: 0, col: 0, priority: 0, coords: new Coords(0, 0) });
         const cameFrom = new Map<string, string>();
-        cameFrom.set([ 0, 0 ].toString(), '');
+        cameFrom.set(new Coords(0, 0).key, '');
         const costSoFar = new Map<string, number>();
-        costSoFar.set([ 0, 0 ].toString(), 0);
+        costSoFar.set(new Coords(0, 0).key, 0);
 
         while (priorityQueue.length) {
             const queue = priorityQueue.sort((a, b) => a.priority - b.priority);
             const current = queue.shift()!;
 
-            if (current.coords === goal.toString()) {
+            if (current.coords.key === goal.key) {
                 break;
             }
 
-            const neighbors = getNeighbors([ current.row, current.col ]);
+            const neighbors = getNeighbors(current.coords);
             for (const neighbor of neighbors) {
-                const newCost = costSoFar.get(current.coords)! + board[neighbor[0]][neighbor[1]];
+                const newCost = costSoFar.get(current.coords.key)! + board[neighbor.row][neighbor.col];
 
-                if (!costSoFar.has(neighbor.toString()) || newCost < costSoFar.get(neighbor.toString())!) {
-                    costSoFar.set(neighbor.toString(), newCost);
-                    priorityQueue.push({ row: neighbor[0], col: neighbor[1], priority: newCost, coords: neighbor.toString() });
-                    cameFrom.set(neighbor.toString(), current.coords);
+                if (!costSoFar.has(neighbor.key) || newCost < costSoFar.get(neighbor.toString())!) {
+                    costSoFar.set(neighbor.key, newCost);
+                    priorityQueue.push({ row: neighbor.row, col: neighbor.col, priority: newCost, coords: neighbor });
+                    cameFrom.set(neighbor.key, current.coords.key);
                 }
             }
         }
 
-        reconstructPath(goal.toString(), [ 0, 0 ].toString(), cameFrom, board);
-    }
-
-    function bfs() {
-        // const target = [ board.length - 1, board[0].length - 1 ];
-        const goal = [ 5, 11 ];
-
-        const front: number[][] = [];
-        front.push([ 0, 0 ]);
-        const cameFrom = new Map<string, string>();
-        cameFrom.set([ 0, 0 ].toString(), '');
-
-        while (front.length) {
-            const current = front.shift()!;
-            const neighbors = getNeighbors(current);
-
-            if (current.toString() === goal.toString()) {
-                break;
-            }
-
-            for (const neighbor of neighbors) {
-                if (!cameFrom.has(neighbor.toString())) {
-                    front.push(neighbor);
-                    cameFrom.set(neighbor.toString(), current.toString());
-                }
-            }
-        }
-
-        reconstructPath(goal.toString(), [ 0, 0 ].toString(), cameFrom, board);
+        reconstructPath(goal.key, new Coords(0, 0).key, cameFrom, board);
     }
 
     // bfs();
