b7b8af1dbb9c9eeb2d347f45925271c2d9a45286
han003 <joar.jacobsen@live.no>
2023-12-26 20:02:44 +0100

    21-1 done

--- 2023/day21/script2.js
+++ 2023/day21/script2.js
@@ -1,13 +1,13 @@
 "use strict";
 (function () {
-    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8');
+    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'example-input.txt'), 'utf-8');
     const start = performance.now();
     const lines = input.split(/\r?\n/).filter((l) => l.length);
-    function getKey(row, col) {
-        return `${row}:${col}`;
+    function getKey(row, column) {
+        return `${row},${column}`;
     }
     const grid = lines.reduce((acc, row, rowIndex) => {
-        row.split('').map((column, columnIndex) => {
+        row.split('').map((_, columnIndex) => {
             acc.set(getKey(rowIndex, columnIndex), {
                 rock: lines[rowIndex].charAt(columnIndex) === '#',
                 garden: lines[rowIndex].charAt(columnIndex) === '#',
@@ -15,43 +15,77 @@
                 row: rowIndex,
                 column: columnIndex,
                 key: getKey(rowIndex, columnIndex),
-                step: 0,
+                infinityRow: 0,
+                infinityColumn: 0,
             });
         });
         return acc;
     }, new Map());
-    let queue = new Map();
-    const visited = new Set();
-    const stepInfo = new Map();
+    const gridRows = lines.length;
+    const gridColumns = lines[0].length;
+    const rocks = Array.from(grid).filter(([_, tile]) => tile.rock).reduce((acc, [_, tile]) => {
+        acc[tile.row] = acc[tile.row] || {};
+        acc[tile.row][tile.column] = true;
+        return acc;
+    }, {});
+    console.log(`rocks`, rocks);
+    let queue = [];
     const startTile = Array.from(grid).find(([_, tile]) => tile.start)?.[1];
     if (!startTile) {
         throw new Error(`No start tile found`);
     }
-    queue.set(startTile.key, startTile);
-    let maxSteps = 64;
-    for (let i = 0; i < maxSteps; i++) {
-        const nextQueue = new Map();
+    queue.push({ row: startTile.row, column: startTile.column, cameFromRow: -1, cameFromColumn: -1 });
+    let maxSteps = 50;
+    let reachable = 0;
+    const reachableTiles = new Set();
+    for (let i = 0; i <= maxSteps; i++) {
+        const nextQueue = [];
+        const canReach = (i % 2) === 0 ? 1 : 0;
+        console.log(`i`, i);
         queue.forEach((tile) => {
+            if (canReach) {
+                reachableTiles.add(`${tile.row},${tile.column}`);
+            }
             const neighbours = [
-                grid.get(getKey(tile.row - 1, tile.column)),
-                grid.get(getKey(tile.row + 1, tile.column)),
-                grid.get(getKey(tile.row, tile.column - 1)),
-                grid.get(getKey(tile.row, tile.column + 1)),
+                { row: tile.row, column: tile.column - 1, cameFromRow: tile.row, cameFromColumn: tile.column },
+                { row: tile.row, column: tile.column + 1, cameFromRow: tile.row, cameFromColumn: tile.column },
+                { row: tile.row - 1, column: tile.column, cameFromRow: tile.row, cameFromColumn: tile.column },
+                { row: tile.row + 1, column: tile.column, cameFromRow: tile.row, cameFromColumn: tile.column },
             ];
             neighbours.forEach((neighbour) => {
-                if (!neighbour) {
+                if (tile.cameFromColumn === neighbour.column && tile.cameFromRow === neighbour.row) {
                     return;
                 }
-                if (neighbour.rock) {
+                const realColumn = neighbour.column < 0 ? (neighbour.column % gridColumns) + gridColumns : neighbour.column % gridColumns;
+                const realRow = neighbour.row < 0 ? (neighbour.row % gridRows) + gridRows : neighbour.row % gridRows;
+                if (rocks[realRow]?.[realColumn]) {
                     return;
                 }
-                neighbour.step = tile.step + 1;
-                nextQueue.set(neighbour.key, neighbour);
+                nextQueue.push(neighbour);
             });
         });
         queue = nextQueue;
+        // console.log(`After ${i} -----------------------------------------`,);
+        //
+        // for (let i = 0; i < lines.length; i++) {
+        //     for (let j = 0; j < lines[i].length; j++) {
+        //         const char = lines[i].charAt(j);
+        //         if (char === 'S') {
+        //             process.stdout.write('S');
+        //         } else if (char === '#') {
+        //             process.stdout.write('#');
+        //         } else if (reachableTiles.has(getKey(i, j))) {
+        //             process.stdout.write('0');
+        //         } else {
+        //             process.stdout.write('.');
+        //         }
+        //     }
+        //
+        //     process.stdout.write('\n');
+        // }
     }
-    console.log(`queue`, queue.size);
+    console.log(`Reach`, reachableTiles.size);
+    console.log(`reachable`, reachable);
     console.log(`Time:`, performance.now() - start);
 })();
 //# sourceMappingURL=script2.js.map
--- 2023/day21/script2.ts
+++ 2023/day21/script2.ts
@@ -1,11 +1,7 @@
 (function () {
-    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'input.txt'), 'utf-8') as string;
+    const input = require('fs').readFileSync(require('path').resolve(__dirname, 'example-input.txt'), 'utf-8') as string;
     const start = performance.now();
     const lines = (input.split(/\r?\n/) as string[]).filter((l) => l.length);
-
-    function getKey(row: number, col: number): string {
-        return `${row}:${col}`;
-    }
 
     type Tile = {
         rock: boolean,
@@ -14,11 +10,18 @@
         row: number,
         column: number,
         key: string,
-        step: number,
+        infinityRow: number;
+        infinityColumn: number;
     };
 
+    type TileData = { row: number, column: number, cameFromRow: number, cameFromColumn: number };
+
+    function getKey(row: number, column: number) {
+        return `${row},${column}`;
+    }
+
     const grid = lines.reduce((acc, row, rowIndex) => {
-        row.split('').map((column, columnIndex) => {
+        row.split('').map((_, columnIndex) => {
             acc.set(getKey(rowIndex, columnIndex), {
                 rock: lines[rowIndex].charAt(columnIndex) === '#',
                 garden: lines[rowIndex].charAt(columnIndex) === '#',
@@ -26,55 +29,97 @@
                 row: rowIndex,
                 column: columnIndex,
                 key: getKey(rowIndex, columnIndex),
-                step: 0,
+                infinityRow: 0,
+                infinityColumn: 0,
             });
         });
 
         return acc;
     }, new Map<string, Tile>());
 
-    let queue = new Map<string, Tile>();
-    const visited = new Set<string>();
-    const stepInfo = new Map<string, number>();
+    const gridRows = lines.length;
+    const gridColumns = lines[0].length;
+
+    const rocks = Array.from(grid).filter(([_, tile]) => tile.rock).reduce((acc, [_, tile]) => {
+        acc[tile.row] = acc[tile.row] || {};
+        acc[tile.row][tile.column] = true;
+        return acc;
+    }, {} as Record<number, Record<number, true>>);
+
+    console.log(`rocks`, rocks);
+
+    let queue: TileData[] = [];
 
     const startTile = Array.from(grid).find(([_, tile]) => tile.start)?.[1];
     if (!startTile) {
         throw new Error(`No start tile found`);
     }
 
-    queue.set(startTile.key, startTile);
+    queue.push({row: startTile.row, column: startTile.column, cameFromRow: -1, cameFromColumn: -1});
 
-    let maxSteps = 64;
+    let maxSteps = 50;
+    let reachable = 0;
+    const reachableTiles = new Set<string>();
 
-    for (let i = 0; i < maxSteps; i++) {
-        const nextQueue = new Map<string, Tile>();
+    for (let i = 0; i <= maxSteps; i++) {
+        const nextQueue: TileData[] = [];
+        const canReach = (i % 2) === 0 ? 1 : 0;
+
+        console.log(`i`, i);
 
         queue.forEach((tile) => {
+            if (canReach) {
+                reachableTiles.add(`${tile.row},${tile.column}`);
+            }
+
             const neighbours = [
-                grid.get(getKey(tile.row - 1, tile.column)),
-                grid.get(getKey(tile.row + 1, tile.column)),
-                grid.get(getKey(tile.row, tile.column - 1)),
-                grid.get(getKey(tile.row, tile.column + 1)),
+                {row: tile.row, column: tile.column - 1, cameFromRow: tile.row, cameFromColumn: tile.column},
+                {row: tile.row, column: tile.column + 1, cameFromRow: tile.row, cameFromColumn: tile.column},
+                {row: tile.row - 1, column: tile.column, cameFromRow: tile.row, cameFromColumn: tile.column},
+                {row: tile.row + 1, column: tile.column, cameFromRow: tile.row, cameFromColumn: tile.column},
             ];
 
             neighbours.forEach((neighbour) => {
-                if (!neighbour) {
+                if (tile.cameFromColumn === neighbour.column && tile.cameFromRow === neighbour.row) {
                     return;
                 }
 
-                if (neighbour.rock) {
+                const realColumn = neighbour.column < 0 ? (neighbour.column % gridColumns) + gridColumns : neighbour.column % gridColumns;
+                const realRow = neighbour.row < 0 ? (neighbour.row % gridRows) + gridRows : neighbour.row % gridRows;
+
+                if (rocks[realRow]?.[realColumn]) {
                     return;
                 }
 
-                neighbour.step = tile.step + 1;
-                nextQueue.set(neighbour.key, neighbour);
+                nextQueue.push(neighbour);
             });
         });
 
         queue = nextQueue;
+
+        // console.log(`After ${i} -----------------------------------------`,);
+        //
+        // for (let i = 0; i < lines.length; i++) {
+        //     for (let j = 0; j < lines[i].length; j++) {
+        //         const char = lines[i].charAt(j);
+        //         if (char === 'S') {
+        //             process.stdout.write('S');
+        //         } else if (char === '#') {
+        //             process.stdout.write('#');
+        //         } else if (reachableTiles.has(getKey(i, j))) {
+        //             process.stdout.write('0');
+        //         } else {
+        //             process.stdout.write('.');
+        //         }
+        //     }
+        //
+        //     process.stdout.write('\n');
+        // }
     }
 
-    console.log(`queue`, queue.size);
+    console.log(`Reach`, reachableTiles.size);
+
+    console.log(`reachable`, reachable);
 
     console.log(`Time:`, performance.now() - start);
 })();
